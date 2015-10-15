23d8ae57ce6594494710ba1b43b680c11c456658
supunkamburugamuva <supun06@gmail.com>
2015-10-15 15:36:07 -0400

    changing results

--- result.py
+++ result.py
@@ -2,7 +2,7 @@ def average(file_name, upperbound):
     with open(file_name, "r") as in_f:
         numbers = []
         for line in in_f:
-            line = line.strip() # remove whitespace
+            line = line.strip().split(',')[0] # remove whitespace
             if line: # make sure there is something there
                 number_on_line = int(line)
                 if number_on_line < upperbound:
@@ -16,37 +16,43 @@ def average(file_name, upperbound):
     return avg_of_numbers
 
 def main():
-    # tasks = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
     tasks = [10, 20, 30, 40, 50, 60]
-    data = [10000, 20000, 40000, 60000, 80000, 100000, 120000, 140000, 160000]
-    # data = [100, 1000, 10000, 20000, 30000]
+    data = [10000, 20000, 40000, 60000, 80000, 100000, 120000, 140000, 160000, 180000, 200000]
+    print 'async binary'
     for d in data:
         s = str(d)
         for t in tasks:
-            s = s + " " + str(average("/home/supun/dev/projects/iotrobots/collectives/jstorm_original_OCT_05/" + str(d)  + "_" + str(t), 1000))
+            s = s + " " + str(average("jstorm/jstorm_bcast_async_binary8x4_OCT_13/" + str(d)  + "_" + str(t), 1000))
         print s
-
-    # for d in data:
-    #     s = str(d)
-    #     for t in tasks:
-    #         s = s + " " + str(average("/home/supun/dev/projects/iotrobots/collectives/jstorm_bcast_SEP_10_binary/" + str(d)  + "_" + str(t), 50))
-    #     print s
-    # for d in data:
-    #     s = str(d)
-    #     for t in tasks:
-    #         s = s + " " + str(average("/home/supun/dev/projects/iotrobots/collectives/jstorm_bcast_SEP_10_binary_logs/" + str(d)  + "_" + str(t), 50))
-    #     print s
+    print 'async flat'
     for d in data:
         s = str(d)
         for t in tasks:
-            s = s + " " + str(average("/home/supun/dev/projects/iotrobots/collectives/jstorm_bcast_OCT_06/" + str(d)  + "_" + str(t), 1000))
+            s = s + " " + str(average("jstorm/jstorm_bcast_async_flat8x4_OCT_13/" + str(d)  + "_" + str(t), 1000))
         print s
-
-    # for d in data:
-    #     s = str(d) + ": "
-    #     for t in tasks:
-    #         s = s + " " + str(average("/home/supun/dev/projects/iotrobots/collectives/bcast/" + str(d)  + "_" + str(t), 60))
-    #     print s
-
+    print 'async pipe'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_bcast_async_pipe8x4_OCT_13/" + str(d)  + "_" + str(t), 1000))
+        print s
+    print 'binary'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_bcast_binary8x4_OCT_13/" + str(d)  + "_" + str(t), 1000))
+        print s
+    print 'flat'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_bcast_flat8x4_OCT_13/" + str(d)  + "_" + str(t), 1000))
+        print s
+    print 'pipe'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_bcast_pipe8x4_OCT_13/" + str(d)  + "_" + str(t), 1000))
+        print s
 if __name__ == "__main__":
     main()
