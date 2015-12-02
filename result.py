a111bb545d6d285ba5d71e106d1a2e1b1df8285c
Supun <supun06@gmail.com>
2015-12-02 12:04:46 -0500

    adding original

--- result.py
+++ result.py
@@ -1,4 +1,5 @@
 def average(file_name, upperbound):
+    avg_of_numbers = 0
     with open(file_name, "r") as in_f:
         numbers = []
         for line in in_f:
@@ -8,10 +9,23 @@ def average(file_name, upperbound):
                 if number_on_line < upperbound:
                     numbers.append(number_on_line)
 
-        avg_of_numbers = 0
+
         if len(numbers) > 0:
             sum_of_numbers = sum(numbers)
-            avg_of_numbers = sum(numbers)/len(numbers)
+            avg_of_numbers = (sum(numbers) + 0.0)/len(numbers)
+
+    with open(file_name, "r") as in_f:
+        numbers = []
+        for line in in_f:
+            line = line.strip().split(',')[0] # remove whitespace
+            if line: # make sure there is something there
+                number_on_line = int(line)
+                if number_on_line < upperbound and number_on_line < avg_of_numbers * 2:
+                    numbers.append(number_on_line)
+
+        if len(numbers) > 0:
+            sum_of_numbers = sum(numbers)
+            avg_of_numbers = (sum(numbers) + 0.0)/len(numbers)
 
     return avg_of_numbers
 
@@ -66,5 +80,54 @@ def main():
         for t in tasks:
             s = s + " " + str(average("jstorm/jstorm_bcast_binary8x4_OCT_19/" + str(d)  + "_" + str(t), 1000))
         print s
+    print 'binary_mapped_30x8x4_NOV_01'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_mapped_binary_30x8x4_NOV_01/" + str(d)  + "_" + str(t), 1000))
+        print s
+    print 'jstorm_bcast_async_binary4x8_NOV_28'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_bcast_async_binary4x8_NOV_28/" + str(d)  + "_" + str(t), 1000))
+        print s
+    print 'binary_mapped_30x8x4_NOV_28'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_bcast_intra_async_binary8x4_NOV_28/" + str(d)  + "_" + str(t), 1000))
+        print s
+    print 'binary_mapped_30x4x8_NOV_28'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_bcast_intra_async_binary4x8_NOV_28/" + str(d)  + "_" + str(t), 1000))
+        print s
+    print 'binary_mapped_30x4x8_NOV_29'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_bcast_intra_async_binary4x8_NOV_29/" + str(d)  + "_" + str(t), 1000))
+        print s
+    print 'binary_mapped_30x4x8_DEC_01'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_bcast_intra_async_binary4x8_DEC_01/" + str(d)  + "_" + str(t), 1000))
+        print s
+    print 'binary_mapped_30x1x8_NOV_30'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_bcast_intra_async_binary1x8_NOV_30/" + str(d)  + "_" + str(t), 1000))
+        print s
+    print 'binary_async_30x1x8_NOV_30'
+    for d in data:
+        s = str(d)
+        for t in tasks:
+            s = s + " " + str(average("jstorm/jstorm_bcast_async_binary1x8_NOV_30/" + str(d)  + "_" + str(t), 1000))
+        print s
+
 if __name__ == "__main__":
     main()
