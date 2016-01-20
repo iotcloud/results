88cfd6bc66009d272e454e77b511cd09be9d0987
supunkamburugamuva <supun06@gmail.com>
2016-01-20 14:00:04 -0500

    adding more results

--- result.py
+++ result.py
@@ -21,7 +21,7 @@ def average(file_name, upperbound):
             if line: # make sure there is something there
                 number_on_line = long(line)
                 # if number_on_line < upperbound and number_on_line < avg_of_numbers * 2:
-                if number_on_line < avg_of_numbers * 10:
+                if number_on_line < avg_of_numbers * 2:
                     numbers.append(number_on_line)
 
         if len(numbers) > 0:
@@ -168,6 +168,13 @@ def large():
     printAverage(tasks, data, 'jstorm_bcast_large_pipesplit30x8x4_DEC_05')
     printAverage(tasks, data, 'jstorm_bcast_large30x8x4_DEC_05')
 
+def large2():
+    tasks = [10, 30, 60]
+    data = [200000, 400000, 600000, 800000, 1000000]
+    printAverage(tasks, data, 'jstorm_bcast_large_intra_binary30x8x4_DEC_08')
+    printAverage(tasks, data, 'jstorm_bcast_large_intra_flat30x8x4_DEC_08')
+    printAverage(tasks, data, 'jstorm_bcast_large_intra_pipesplit30x8x4_DEC_08')
+
 
 def normal():
     tasks = [10, 20, 30, 40, 50, 60]
@@ -198,19 +205,20 @@ def normal2():
 
 
 def calcHist():
-    histogram(10, 'jstorm_bcast_intra_binary30x8x4_DEC_08', 60, 200000)
-    histogram(10, 'jstorm_bcast_intra_flat30x8x4_DEC_08', 60, 200000)
-    histogram(10, 'jstorm_bcast_intra_pipesplit30x8x4_DEC_08', 60, 200000)
+    histogram(50, 'jstorm_bcast_intra_binary30x8x4_DEC_08', 60, 200000)
+    histogram(50, 'jstorm_bcast_intra_flat30x8x4_DEC_08', 60, 200000)
+    histogram(50, 'jstorm_bcast_intra_pipesplit30x8x4_DEC_08', 60, 200000)
 
-    histogram(10, 'jstorm_bcast_intra_binary30x8x4_DEC_08', 30, 200000)
-    histogram(10, 'jstorm_bcast_intra_flat30x8x4_DEC_08', 30, 200000)
-    histogram(10, 'jstorm_bcast_intra_pipesplit30x8x4_DEC_08', 30, 200000)
+    histogram(50, 'jstorm_bcast_intra_binary30x8x4_DEC_08', 30, 200000)
+    histogram(50, 'jstorm_bcast_intra_flat30x8x4_DEC_08', 30, 200000)
+    histogram(50, 'jstorm_bcast_intra_pipesplit30x8x4_DEC_08', 30, 200000)
 
 def histogram(binNo, directory, task, data):
     avg_of_numbers = 0
     min_of_numbers = 0
     max_of_numbers = 0
     fileName = "jstorm/" + directory + "/" + str(data)  + "_" + str(task)
+    print fileName
     with open(fileName, "r") as in_f:
         numbers = []
         maxes = []
@@ -259,13 +267,17 @@ def histogram(binNo, directory, task, data):
                         if (number_on_line < bin["end"]):
                             bin["count"] += 1
                             break
-    print bins
+
+    for i in range(0, binNo):
+        bin = bins[i]
+        print str(bin["end"]) + "," + str(bin["count"])
 
 def main():
     pass
 
 if __name__ == "__main__":
     calcHist()
+    #large2()
     #normal2()
     # large()
     #mma2()
